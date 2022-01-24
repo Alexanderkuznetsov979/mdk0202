28d4387ca6b29fa80738cdb1b524fa689ecb66fe
Alexanderkuznetsov979 <73643953+Alexanderkuznetsov979@users.noreply.github.com>
2022-01-24 12:33:27 +0300

    Add files via upload

---
+++ blocks/nav1.php
@@ -0,0 +1,17 @@
+<nav>
+	<ul>
+		<li><a href="">Кровати</a></li>
+	</ul>
+	<ul>
+		<li><a href="">Диваны</a></li>
+	</ul>
+	<ul>
+		<li><a href="">Матрасы</a></li>
+	</ul>
+	<ul>
+		<li><a href="">Кухни</a></li>
+	</ul>
+	<ul>
+		<li><a href="">Контакты</a></li>
+	</ul>
+</nav>
